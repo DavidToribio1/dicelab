907904d9fa9ad0f6e3ed57737aa10b2a5ab983ef
DavidToribio1 <dtmuzik1@gmail.com>
2020-11-12 20:18:56 -0500

    added dice methods and bin method

--- src/main/java/Bins.java
+++ src/main/java/Bins.java
@@ -1,4 +1,35 @@
 
-public class Bins {
+import java.util.ArrayList;
 
+public class Bins {
+    //declare variables
+    private Integer minRoll;
+    private Integer maxRoll;
+    int[] binArray;
+
+    //instantiated
+    public Bins(int minRoll, int maxRoll){
+        this.maxRoll = maxRoll;
+        this.minRoll = minRoll;
+        binArray = new int[maxRoll];
+
+
+
+    }
+    //
+    public void incrementBin(int result){
+        binArray[result-2]++;
+    }
+    public Integer getBin(int binId)
+    {
+        return binArray[binId];
+    }
+
+    public Integer getBinMin(){
+        return minRoll;
+    }
+    public Integer getBinMax(){
+        return maxRoll;
+    }
 }
+
--- src/main/java/Dice.java
+++ src/main/java/Dice.java
@@ -1,4 +1,23 @@
 public class Dice {
 
+        private Integer numberOfDice;
+        private Integer tossAndSum = 0;
+
+        public Dice(int numberOfDice){
+            this.numberOfDice = numberOfDice;
+        }
+
+        public Integer tossAndSum(){
+            tossAndSum = 0;
+            for(int i=1; i<= numberOfDice; i++){
+                tossAndSum += 1+(int)(Math.random()*6);
+            }
+            return tossAndSum;
+
+        }
+        public Integer getNumberOfDice(){
+            return numberOfDice;
+        }
+
 
 }
